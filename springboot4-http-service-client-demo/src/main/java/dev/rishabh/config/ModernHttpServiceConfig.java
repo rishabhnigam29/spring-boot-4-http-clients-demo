af5eb2b7e769c8b99999e150c5a366c99267ba90
Rishabh <rishabhkumarnigam@gmail.com>
2025-12-19 17:45:21 +0530

    Updated configs

--- springboot4-http-service-client-demo/src/main/java/dev/rishabh/config/ModernHttpServiceConfig.java
+++ springboot4-http-service-client-demo/src/main/java/dev/rishabh/config/ModernHttpServiceConfig.java
@@ -1,5 +1,6 @@
 package dev.rishabh.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
@@ -10,18 +11,20 @@ import org.springframework.web.service.registry.ImportHttpServices;
 @ImportHttpServices(group = "stackoverflow", basePackages = "dev.rishabh.service.stackoverflow")
 public class ModernHttpServiceConfig {
 
-//    @Bean
-//    RestClientHttpServiceGroupConfigurer groupConfigurer(){
-//        return groups -> {
-//            //Configure GitHub group
-//            groups.filterByName("github")
-//                    .forEachClient((client,builder)->
-//                            builder.baseUrl("https://api.github.com"));
-//
-//            //Configure StackOverflow group
-//            groups.filterByName("stackoverflow")
-//                    .forEachClient((client,builder)->
-//                            builder.baseUrl("https://api.stackexchange.com/2.3"));
-//        };
-//    }
+    @Bean
+    RestClientHttpServiceGroupConfigurer groupConfigurer(
+            @Value("${github.base.url}") String githubBaseUrl,
+            @Value("${stackoverflow.base.url}") String stackOverFlowBaseUrl){
+        return groups -> {
+            //Configure GitHub group
+            groups.filterByName("github")
+                    .forEachClient((client,builder)->
+                            builder.baseUrl(githubBaseUrl));
+
+            //Configure StackOverflow group
+            groups.filterByName("stackoverflow")
+                    .forEachClient((client,builder)->
+                            builder.baseUrl(stackOverFlowBaseUrl));
+        };
+    }
 }
